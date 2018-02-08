06d8a6c130c1e87f9976a4868f2c12ba09b0a30a
Raimon Esteve <raimon@nan-tic.com>
2018-02-08 14:47:08 +0100

    Style register class and XML header

--- __init__.py
+++ __init__.py
@@ -1,8 +1,9 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 from trytond.pool import Pool
-import configuration
-import distribution
+from . import configuration
+from . import distribution
+
 
 def register():
     Pool.register(
